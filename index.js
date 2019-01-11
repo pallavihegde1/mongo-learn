64d1d9728030fd0f3a3f76d2fcd6f8059e98b681
pallavi-syook <pallavi@syook.com>
2019-01-11 11:51:56 +0530

    mongo read and create and filter queries

---
+++ index.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+//connect to a mongo db database
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('connected to mongo db'))
+  .catch(err => console.error('could not connect to mongo db'));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],// it will stored as key value pairs key is index and values will be these strings
+  date: { type: Date, default: Date.now}, // if we need to specify default values
+  isPublished: Boolean
+})
+
+const Course = mongoose.model('Course', courseSchema);
+
+//create
+async function createCourse() {
+  const course = new Course({
+    name: 'node course',
+    author: 'mosh',
+    tags: ['node', 'mongoose'],
+    isPublished: true
+  })
+
+  const result = await course.save();
+  console.log(result)
+}
+
+createCourse()
+
+
+// read all
+
+async function getCourses() {
+  const courses = await Course.find().sort({name: 1})
+  console.log(courses)
+}
+
+getCourses()
+
+async function getFilteredCourses() {
+  const courses = await Course.find({author: 'mish', isPublished: true})
+  console.log('filtered Courses', courses)
+}
+
+getFilteredCourses()
